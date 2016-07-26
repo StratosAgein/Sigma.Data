3e34d389a14b49cd1aef4874bbcb9e1e7e04b9c0
Cami Rodriguez <camiepisode@outlook.com>
2016-07-26 12:51:47 -0500

    Corrected script

--- CreateTestUser.js
+++
@@ -1,7 +0,0 @@
-var conn = new Mongo();
-db = conn.getDB("SigmaDB");
-
-db.adminCommand('listDatabases')
-
-	
-db.getCollectionNames()
---
+++ SeedUsers.js
@@ -0,0 +1,10 @@
+db.User.insert({
+    name:'Camilo Rodriguez',
+    email:'camiepisode@outlook.com',
+    roles:[
+        {
+            name:'superadmin',
+        }
+    ],
+    age: 23
+})
