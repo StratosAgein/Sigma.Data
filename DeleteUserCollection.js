fd9c6d1a500472e7623d125fa684b7c34d2de437
Cami Rodriguez <camiepisode@outlook.com>
2016-07-26 14:29:16 -0500

    Added more scripts

---
+++ DeleteUserCollection.js
@@ -0,0 +1,3 @@
+// DB: 127.0.0.1/SigmaDB
+
+db.User.remove({});
--- SeedUsers.js
+++ SeedUsers.js
@@ -1,6 +1,9 @@
+// DB: 127.0.0.1/SigmaDB
 db.User.insert({
+    "_id" : ObjectId("51e0373c6f35bd826f47e9a1"),
     name:'Camilo Rodriguez',
     email:'camiepisode@outlook.com',
+    password:'CAMILO',
     roles:[
         {
             name:'superadmin',
