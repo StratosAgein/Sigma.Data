762ee176ab464d169ce4567a4223de8ef1032cca
Cami Rodriguez <camiepisode@outlook.com>
2016-07-29 11:09:50 -0500

    Improving seed

--- SeedUsers.js
+++ SeedUsers.js
@@ -1,13 +1,30 @@
 // DB: 127.0.0.1/SigmaDB
+//
+// Status:
+//      0 : Disabled
+//      1 : Active
+//      2 : Inactive
+//      3 : Deleted
+//
+
 db.User.insert({
-    "_id" : ObjectId("51e0373c6f35bd826f47e9a1"),
-    name:'Camilo Rodriguez',
-    email:'camiepisode@outlook.com',
-    password:'CAMILO',
-    roles:[
-        {
-            name:'superadmin',
-        }
+    "_id" :     ObjectId("51e0373c6f35bd826f47e9a1"),
+    Name:       'Camilo Rodriguez',
+    Lastname:   'Rodriguez',
+    Email:      'camiepisode@outlook.com',
+    Password:   'CAMILO',
+    Status:     1,
+    Phone:      '+573103494806',
+    Roles:
+    [
+                {
+                    Group:      'superadmin',
+                    IsPrimary: true
+                },
+                {
+                    Group:      'buyer',
+                    IsPrimary: false
+                }
     ],
     age: 23
 })
