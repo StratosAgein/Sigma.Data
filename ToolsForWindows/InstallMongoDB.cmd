b2ea1d83a0711d763f2d76f3c427283d0df22ffa
Cami Rodriguez <camiepisode@outlook.com>
2016-08-01 15:49:18 -0500

    Added to regedit

--- ToolsForWindows/InstallMongoDB.cmd
+++ ToolsForWindows/InstallMongoDB.cmd
@@ -5,3 +5,5 @@ echo Welcome to Sigma DevOps Platform
 choco install mongodb -y
 
 setx path "%path%;%ProgramFiles%\MongoDB\Server\3.2\bin\"
+
+reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MongoDB_Server" /t REG_SZ /d "%ProgramFiles%\MongoDB\Server\3.2\bin\mongod.exe" /f
