631389738c6bf34e121cb46fad2bde6d9c26c031
Cami Rodriguez <camiepisode@outlook.com>
2016-08-01 15:41:57 -0500

    Added path to environment

--- ToolsForWindows/InstallMongoDB.cmd
+++ ToolsForWindows/InstallMongoDB.cmd
@@ -3,3 +3,5 @@ echo Welcome to Sigma DevOps Platform
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 choco install mongodb -y
+
+setx path "%path%;%ProgramFiles%\MongoDB\Server\3.2\bin\"
