91fe80b53e9172c0b5500a8cc4ebd9ee18be4583
Cami Rodriguez <camiepisode@outlook.com>
2016-08-01 15:59:34 -0500

    Hidding output

--- ToolsForWindows/InstallMongoDB.cmd
+++ ToolsForWindows/InstallMongoDB.cmd
@@ -1,3 +1,4 @@
+@echo off
 echo Welcome to Sigma DevOps Platform
 
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
